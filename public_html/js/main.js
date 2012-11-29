91be1c313eb497b0fc58ef08eccfd794236ddfad
Meathill <meathill@gmail.com>
2012-11-29 19:00:58 +0800

    尝试几个tween无果，还是用jq

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,23 @@
-
+$(function () {
+  function createNextCard() {
+    var thiscard = createCard();
+    thiscard.addClass('rotation');
+    cards.push(thiscard);
+    setTimeout(function () {
+      thiscard.removeClass('rotation');
+    }, 2000);
+    if (cards.length < CARDS_NUM) {
+      setTimeout(createNextCard, 50);
+    }
+  }
+  var card = createCard(),
+      cards = [];
+  card
+    .css('top', '-200px')
+    .animate({top: 100}, 1000, 'easeOutBounce', createNextCard);
+  cards.push(card);
+});
+function createCard(init) {
+  return $('<div class="card card-back"></div>').appendTo('#cards');
+}
+var CARDS_NUM = 30;
