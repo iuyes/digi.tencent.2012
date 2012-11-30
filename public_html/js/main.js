a8290b8b75527280f0d988f3a70d5539d73914d2
Meathill <meathill@gmail.com>
2012-11-30 09:27:13 +0800

    继续尝试中
    Signed-off-by: Meathill <meathill@gmail.com>

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,23 +1,42 @@
 $(function () {
   function createNextCard() {
-    var thiscard = createCard();
-    thiscard.addClass('rotation');
-    cards.push(thiscard);
-    setTimeout(function () {
-      thiscard.removeClass('rotation');
-    }, 2000);
+    playRotation($(this));
+    
     if (cards.length < CARDS_NUM) {
-      setTimeout(createNextCard, 50);
+      var thiscard = createCard();
+      cards.push(thiscard);
+      setTimeout($.proxy(createNextCard, thiscard), 100);
+    } else {
+      setTimeout(listCards, 2200);
     }
   }
-  var card = createCard(),
-      cards = [];
+  function playRotation(card) {
+    card.addClass('rotation');
+    setTimeout(function () {
+      card.removeClass('rotation');
+    }, 2000);
+  }
+  function listCards() {
+    for (var i = 0; i < CARDS_NUM; i++) {
+      cards[i]
+        .addClass('card-ready')
+        .css('-webkit-transition', 'left-' + (100 + i * 45) + 'px 0.4 ease-in-out ' + i / 20);
+    }
+  }
+
+  var card = createCard();
   card
     .css('top', '-200px')
-    .animate({top: 100}, 1000, 'easeOutBounce', createNextCard);
+    .animate({top: 100}, 1000, 'easeOutBounce', function () {
+      $(this).attr('style', '');
+      createNextCard.call(this);
+    });
   cards.push(card);
 });
 function createCard(init) {
-  return $('<div class="card card-back"></div>').appendTo('#cards');
+  var card = $('<div class="card card-back"></div>').appendTo('#cards');
+  card.css('z-index', CARDS_NUM - cards.length);
+  return card;
 }
-var CARDS_NUM = 30;
+var cards = [],
+    CARDS_NUM = 20;
