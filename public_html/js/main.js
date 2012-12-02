c170867615a6a5045393f3ce9bf954bad0076e64
Meathill <meathill@gmail.com>
2012-12-03 00:32:12 +0800

    初步完工，开始使用tweenlite
    Signed-off-by: Meathill <meathill@gmail.com>

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -17,46 +17,81 @@ $(function () {
     }, 2000);
   }
   function listCards() {
-    for (var i = 0; i < CARDS_NUM; i++) {
-      cards[i]
-        .addClass('card-ready')
-        .delay(i * 50)
-        .animate({
-          top: 300,
-          left: 100 + 45 *i,
-        }, 400, 'easeInOutCubic');
-    }
+    var COLS = 9,
+        row = 0,
+        col = 0,
+        interval = 0,
+        count = 0
+        left = ($(window).width() - 760 >> 1) + 60;
+    interval = setInterval(function () {
+      col = count % COLS;
+      row = count / COLS >> 0;
+      TweenLite.to(cards[CARDS_NUM - count - 1][0], 0.4, {
+        css: {
+          left: left + col * 80,
+          top: 150 + row * 40 + Math.cos((col - 4) * Math.PI / 8) * 30,
+          rotation: (4 - col) * 10,
+        },
+        ease: Cubic.easeInOut,
+        onStart: function () {
+          $(this.target).removeClass('card-back');
+        },
+        onComplete: function () {
+          $(this.target).addClass('card-ready');
+        },
+      });
+      count++;
+      if (count == CARDS_NUM) {
+        clearInterval(interval);
+      }
+    }, 50);
   }
-
+  
+  template = $('#template').html();
   var card = createCard();
-  card
-    .css('top', '-200px')
-    .animate({top: 100}, 1000, 'easeOutBounce', createNextCard);
+  card.addClass('start');
+  TweenLite.to(card[0], 1, {css: {top: 100}, ease: Bounce.easeOut, onComplete: $.proxy(createNextCard, card)});
   cards.push(card);
 });
 function createCard(init) {
-  var card = $('<div class="card card-back"></div>').appendTo('#cards');
+  var card = $(template).appendTo('#cards');
   card.css('z-index', CARDS_NUM - cards.length);
   return card;
 }
 var cards = [],
-    CARDS_NUM = 20;
+    template = '',
+    CARDS_NUM = 54;
 
 $(document)
   .on('mouseover', '.card-ready', function (event) {
+    TweenLite.to(this, 0.5, {css: {
+      scale: 1.2,
+    }, ease: Cubic.easeInOut});
+    if ($(this).css('z-index') != CARDS_NUM) {
+      $(this).data('z-index', $(this).css('z-index'))
+    }
     $(this)
-      .animate({
-        top: 280,
-      })
-      .addClass('zoom-in')
-      .data('level', $(this).css('z-index'))
-      .css('z-index', CARDS_NUM + 1);
+      .css('z-index', CARDS_NUM);
   })
   .on('mouseout', '.card-ready', function (event) {
-    $(this)
-      .animate({
-        top: 300,
-      })
-      .removeClass('zoom-in')
-      .css('z-index', $(this).data('level'));
+    TweenLite.to(this, 0.3, {
+      css: {
+        scale: 1,
+      },
+      ease: Cubic.easeOut,
+      onComplete: function () {
+        $(this.target)
+          .css('z-index', $(this.target).data('z-index'));
+      }
+    });
+  })
+  .on('click', '.device-button', function (event) {
+    $('#device-detail').data('card', $(this).closest('.card'));
+  })
+  .on('click', 'button[type=submit]', function (event) {
+    var modal = $(this).closest('.modal'),
+        card = modal.data('card');
+    card.removeClass('card-ready').attr('style', '');
+    $('#fav').append(card);
+    modal.modal('hide');
   })
