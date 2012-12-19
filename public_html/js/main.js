86f3c1c4b8e6cc895e56723a17931e90a253c2ed
Meathill <meathill@gmail.com>
2012-12-19 22:52:59 +0800

    基本调整完毕
    Signed-off-by: Meathill <meathill@gmail.com>

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,13 +1,14 @@
 $(function () {
   function createBox(center) {
     var box = $('<div class="card-box"></div>').appendTo('#cards');
-    return box[0];
+    return box;
   }
   function createCard() {
     var container = $('#cards'),
         index = container.children().length - 1,
         card = $(cardTemplate).appendTo(container);
-    card.find('img').attr('src', 'images/cards/card' + index + '.jpg');
+    card.attr('title', DEVICES[index].name)
+      .find('img').attr('src', 'images/cards/card' + index + '.jpg');
     return card;
   }
   function createNextCard() {
@@ -44,13 +45,13 @@ $(function () {
     if (index < 2) {
       obj.scale = 1.3;
       obj.left = 365 + index * 156;
-      obj.top = 115;
+      obj.top = 240;
     } else if ((index - 2) % 13 < 2) {
       obj.left = 73 + ((index - 2) % 13 + (index / 13 >> 0) * 2) * 102 + (index / 13 > 2 ? 26 : 0);
-      obj.top = 278;
+      obj.top = 400;
     } else {
       obj.left = (index - 4 - ((index - 2) / 13 >> 0) * 2) % 11 * 74 + 73;
-      obj.top = ((index - 4 - ((index - 2) / 13 >> 0) * 2) / 11 >> 0) * 60 + 420;
+      obj.top = ((index - 4 - ((index - 2) / 13 >> 0) * 2) / 11 >> 0) * 40 + 540;
     }
     var animation = {
       css: obj
@@ -68,9 +69,9 @@ $(function () {
     $('#tidy-button a').removeClass('disabled');
   }
   function addCardToFav(card) {
-    if (favcards.children().length == 8) {
+    if (favcards.children().length == 12) {
       $('#fav-cards').popover('show');
-      setTimout(function () {
+      setTimeout(function () {
         $('#fav-cards').popover('hide');
       }, 3000);
     }
@@ -81,7 +82,7 @@ $(function () {
         left: (favcards.children().length - 1) * 70,
         top: 24,
       });;
-    TweenLite.to(card[0], 0.5, {
+    TweenLite.to(card, 0.5, {
       css: {
         scale: 1,
         rotation: 0,
@@ -90,22 +91,31 @@ $(function () {
     favcards.removeClass('active');
     for (var i =0; i < CARDS_NUM; i++) {
       if (card.is(cards[i])) {
-        money += PRICES[i];
+        money += DEVICES[i].price;
         break;
       }
     }
     displayNumbers(money);
   }
-  function removeCardFromFav(card) {
+  function removeCardFromFav(card, hasAnimation) {
     var offset = favcards.offset();
     card.css({
       left: '+=30',
       top: '+=' + offset.top,
     });
     $('#cards').append(card);
+    if (hasAnimation) {
+      var radix = Math.random() / 2,
+          angle = Math.random() * Math.PI * 2;
+      TweenLite.to(card, 0.5, {
+        left: viewport.width * Math.cos(angle) * radix + viewport.width / 2,
+        top: viewport.height * Math.sin(angle) * radix + viewport.cY,
+        rotation: Math.random() * 90 - 45, 
+      });
+    }
     for (var i =0; i < CARDS_NUM; i++) {
       if (card.is(cards[i])) {
-        money -= PRICES[i];
+        money -= DEVICES[i].price;
         break;
       }
     }
@@ -120,7 +130,7 @@ $(function () {
     }
     while (number > 0) {
       $('<li class="num' + number % 10 +'"></li>').prependTo(ul);
-      number = number / 10;
+      number = number / 10 >> 0;
     }
   }
   function init() {
@@ -164,14 +174,25 @@ $(function () {
     });
     $(document)
       .on('click', '.device-button', function (event) {
+        var card = $(this).closest('.card'),
+            fromFav = card.parent().is('#fav-cards');
         $('#device-detail')
-          .data('card', $(this).closest('.card'))
-          .find('.modal-body').html($(this).children().first().clone());
+          .data('card', card)
+          .find('h3').html(card.attr('title'))
+          .end().find('.modal-body').html($(this).children().first().clone())
+          .end().find('.btn-success').toggle(!fromFav)
+          .end().find('.btn-danger').toggle(fromFav);
       })
-      .on('click', 'button[type=submit]', function (event) {
+      .on('click', '.btn-success', function (event) {
         var modal = $(this).closest('.modal'),
             card = modal.data('card');
         addCardToFav(card);
+        modal.modal('hide');
+      })
+      .on('click', '.btn-danger', function (event) {
+        var modal = $(this).closest('.modal'),
+            card = modal.data('card');
+        removeCardFromFav(card, true);
         modal.modal('hide');
       })
       .on('click', '#tidy-button a', function (event) {
@@ -183,7 +204,7 @@ $(function () {
       });
     $('#fama, #tidy-button').show();
     TweenLite.to(box, 0.5, {css: {top: -120, left: 60}});
-    TweenLite.to($('#title')[0], 0.5, {css: {top: -12}, ease: Cubic.easeOut});
+    TweenLite.to($('#title'), 0.5, {css: {top: 20}, ease: Cubic.easeOut});
   }
   
   // 动画由此开始
@@ -197,12 +218,12 @@ $(function () {
       garbage = $('#garbage'),
       favcards = $('#fav-cards');
   
-  $('#cards').height(viewport.height);
+  $('#cards').height(viewport.height + 200);
   TweenLite.to(box, 0.5, {css: {top: -60, left: 120}, ease: Back.easeOut, onComplete: createNextCard, delay: 0.5});
   displayNumbers(money);
 });
 var CARDS_NUM = 54,
-    PRICES = ["5288", "248", "16488", "13999", "7999", "7388", "9699", "9999", "6999", "28999", "10988", "10999", "9688", "7999", "129", "750", "18499", "169999", "17499", "13199", "8000", "2999", "228", "2100", "3480", "890", "699", "1150", "4599", "3599", "4899", "4799", "3999", "1880", "3500", "2998", "2499", "1999", "2499", "399", "199", "2498", "3759", "3688", "23499", "1300", "9300", "4488", "5499", "3328", "15399", "1300", "3299", "33566"];
+    DEVICES = [{"name":"iPhone5","price":5288},{"name":"Windows 8","price":248},{"name":"苹果Retina MacBook Pro 15","price":16488},{"name":"索尼Z13","price":13999},{"name":"东芝Z830","price":7999},{"name":"苹果MacBook Air","price":7388},{"name":"ThinkPad X1 Carbon","price":9699},{"name":"宏碁S7","price":9999},{"name":"联想YOGA","price":6999},{"name":"戴尔 Alienware M18x","price":28999},{"name":"华硕太极","price":10988},{"name":"戴尔XPS 12","price":10999},{"name":"苹果iMac","price":9688},{"name":"索尼VAIO Tap 20","price":7999},{"name":"TP-Link WR720N 3G无线路由器","price":129},{"name":"Amazon Kindle Paperwhite","price":750},{"name":"SONY 55HX950","price":18499},{"name":"LG 84LM9600","price":169999},{"name":"三星UA55ES8000","price":17499},{"name":"夏普LCD-52LX840A","price":13199},{"name":"SONOS无线音箱系统","price":8000},{"name":"罗技UE900","price":2999},{"name":"苹果earpods","price":228},{"name":"任天堂WII U","price":2100},{"name":"iRobot Roomba扫地机器人","price":3480},{"name":"Garmin 2500","price":890},{"name":"菲比电子宠物","price":699},{"name":"Nike+Fuelband","price":1150},{"name":"诺基亚Lumia920","price":4599},{"name":"三星GALAXY S III","price":3599},{"name":"三星GALAXY Note II","price":4899},{"name":"HTC Butterfly","price":4799},{"name":"HTC 8X","price":3999},{"name":"Google Nexus 4","price":1880},{"name":"索尼LT29i","price":3500},{"name":"OPPO Find 5","price":2998},{"name":"魅族MX2","price":2499},{"name":"小米手机2","price":1999},{"name":"步步高 VIVO X1","price":2499},{"name":"三洋爱乐普MB-L2DTC移动应急电源","price":399},{"name":"拉卡拉Q2手机刷卡器","price":199},{"name":"苹果iPad mini","price":2498},{"name":"索尼RX1","price":3759},{"name":"苹果iPad 4","price":3688},{"name":"佳能5D Mark III","price":23499},{"name":"Google Nexus7","price":1300},{"name":"富士X-E1","price":9300},{"name":"微软Surface RT","price":4488},{"name":"索尼NEX-6","price":5499},{"name":"三星Galaxy Note 10.1","price":3328},{"name":"尼康D600","price":15399},{"name":"Amazon Kindle Fire HD 8.9","price":1300},{"name":"三星GALAXY Camera","price":3299},{"name":"索尼NEX-VG900E","price":33566}];
 function createViewport(w, h) {
   var viewport = {
     width: 860,
@@ -210,9 +231,9 @@ function createViewport(w, h) {
     cX: 0,
     cY: 0,
   };
-  viewport.height = (h < 1040 ? (h > 700 ? h : 700) : 1040) - 200;
+  viewport.height = (h < 1040 ? (h > 800 ? h : 800) : 1040) - 200;
   viewport.cX = viewport.width >> 1;
-  viewport.cY = (viewport.height - 200 >> 1) + 200;
+  viewport.cY = (viewport.height - 200 >> 1) + 180;
   return viewport;
 }
 $.fn.hitTestObject = function(obj){
