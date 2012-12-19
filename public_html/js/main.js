a504e9aeaf6ca40cee1361cabc6a19fff0772de8
Meathill <meathill@gmail.com>
2012-12-19 16:14:56 +0800

    完成拖动

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -18,8 +18,9 @@ $(function () {
     TweenLite.to(thiscard, 0.4, {
       css: {
         left: viewport.width * Math.cos(angle) * radix + viewport.width / 2,
-        top: viewport.height * Math.sin(angle) * radix + viewport.cY + 150,
+        top: viewport.height * Math.sin(angle) * radix + viewport.cY,
         rotation: Math.random() * 90 - 45,
+        z: 1,
       },
       onStart: function () {
         $(this.target).toggleClass('card-ready card-back');
@@ -37,22 +38,18 @@ $(function () {
     if ($.contains($('#fav-cards')[0], card)) {
       return;
     }
-    if (index == CARDS_NUM) {
-      $('#tidy-button').removeClass('disabled');
-      return;
-    }
     var obj = {
       rotation: 0,
     };
     if (index < 2) {
       obj.scale = 1.3;
-      obj.left = 364 + index * 158;
+      obj.left = 365 + index * 156;
       obj.top = 115;
     } else if ((index - 2) % 13 < 2) {
-      obj.left = 72 + ((index - 2) % 13 + (index / 13 >> 0) * 2) * 102 + (index / 13 > 2 ? 28 : 0);
+      obj.left = 73 + ((index - 2) % 13 + (index / 13 >> 0) * 2) * 102 + (index / 13 > 2 ? 26 : 0);
       obj.top = 278;
     } else {
-      obj.left = (index - 4 - ((index - 2) / 13 >> 0) * 2) % 11 * 74 + 72;
+      obj.left = (index - 4 - ((index - 2) / 13 >> 0) * 2) % 11 * 74 + 73;
       obj.top = ((index - 4 - ((index - 2) / 13 >> 0) * 2) / 11 >> 0) * 60 + 420;
     }
     var animation = {
@@ -61,8 +58,14 @@ $(function () {
     if (index < CARDS_NUM - 1) {
       animation.onComplete = tidyNextCard;
       animation.onCompleteParams = [cards[index + 1]];
+    } else {
+      animation.onComplete = tidyOver();
     }
+    $('#cards').append(card);
     TweenLite.to(card, 0.2, animation);
+  }
+  function tidyOver() {
+    $('#tidy-button a').removeClass('disabled');
   }
   function addCardToFav(card) {
     card
@@ -77,12 +80,58 @@ $(function () {
   }
   function init() {
     $('.card-ready').draggable({
-      cursor: 'crosshair',
       snap: '#fav-cards',
       snapMode: 'inter',
-      start: function (event, ui) {
-        $('#cards').append(ui.helper);
+      drag: function (event, ui) {
+        if (ui.helper.hitTestObject(garbage)) {
+          garbage.addClass('active');
+          return;
+        }
+        if (ui.helper.hitTestObject(favcards)) {
+          favcards.addClass('active');
+          return;
+        }
+        $('.active').removeClass('active');
       },
+      start: function (event, ui) {
+        ui.helper.addClass('top-card');
+      },
+      stop: function (event, ui) {
+        ui.helper.removeClass('top-card');
+        if (garbage.hasClass('active')) {
+          ui.helper
+            .draggable('destroy')
+            .remove();
+          garbage.removeClass('active');
+          return;
+        }
+      
+        if (favcards.hasClass('active')) {
+          ui.helper
+            .appendTo(favcards)
+            .css({
+              left: (favcards.children().length - 1) * 70,
+              top: 24,
+            });
+          TweenLite.to(ui.helper[0], 0.3, {
+            css: {
+              scale: 1,
+              rotation: 0,
+            },
+          });
+          favcards.removeClass('active');
+          return;
+        }
+      
+        if (ui.helper.parent().is(favcards)) {
+          var offset = favcards.offset();
+          ui.helper.css({
+            left: '+=30',
+            top: '+=' + offset.top,
+          });
+        }
+        $('#cards').append(ui.helper);
+      }
     });
     $(document)
       .on('click', '.device-button', function (event) {
@@ -96,7 +145,7 @@ $(function () {
         addCardToFav(card);
         modal.modal('hide');
       })
-      .on('click', '#tidy-button', function (event) {
+      .on('click', '#tidy-button a', function (event) {
         if ($(this).hasClass('disabled')) {
           return;
         }
@@ -114,9 +163,11 @@ $(function () {
       HEIGHT = $(window).height(),
       viewport = createViewport(WIDTH, HEIGHT - 180);
       cardTemplate = $('#card-template').html(),
-      box = createBox(viewport.cX);
+      box = createBox(viewport.cX),
+      garbage = $('#garbage'),
+      favcards = $('#fav-cards');
   
-  $('#cards').height(viewport.height + 90);
+  $('#cards').height(viewport.height + 200);
   TweenLite.to(box, 0.5, {css: {top: -60, left: 120}, ease: Back.easeOut, onComplete: createNextCard, delay: 0.5});
 });
 var CARDS_NUM = 54;
@@ -124,16 +175,23 @@ function createViewport(w, h) {
   var viewport = {
     width: 860,
     height: 0,
-    x: 0,
-    y: 0,
     cX: 0,
     cY: 0,
   };
-  viewport.height = h < 750 ? (h > 500 ? h : 500) : 750;
-  viewport.x = w - viewport.width >> 1;
-  viewport.y = h - viewport.height >> 1;
-  viewport.cX = w >> 1;
-  viewport.cY = h - 320 >> 1;
-  console.log(viewport.height, viewport.cY);
+  viewport.height = (h < 1040 ? (h > 700 ? h : 700) : 1040) - 200;
+  viewport.cX = viewport.width >> 1;
+  viewport.cY = (viewport.height - 200 >> 1) + 200;
   return viewport;
+}
+$.fn.hitTestObject = function(obj){
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+     
+    var compare = obj.offset();
+    compare.right = compare.left + obj.outerWidth();
+    compare.bottom = compare.top + obj.outerHeight();
+             
+    return (!(compare.right < bounds.left || compare.left > bounds.right || compare.bottom < bounds.top || compare.top > bounds.bottom));
+     
 }
